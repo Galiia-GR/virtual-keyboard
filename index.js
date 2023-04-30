308fa0a8282fee721fffdafa7b498acdef97c115
Galiia-GR <hope05590@gmail.com>
2023-04-30 21:00:53 +0600

    feat: add in layout dicriptions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { drawKeyboardRu } from './drawKeyboard.js';
+import { drawKeyboardRu } from './drawKeyboardRu.js';
 
 function layout() {
 const {body}=document;
@@ -11,10 +11,18 @@ container.innerHTML = `
 <h1 class="keyboard__title"></h1>
 <textarea class=container__textarea></textarea>
 <div class="keyboard"></div>
+<p class="description"></p>
+<p class="change__lang"></p>
 `;
 
 document.querySelector(".keyboard__title")
 .innerText="RSS Виртуальная клавиатура";
+
+document.querySelector(".description")
+.innerText="Клавиатура создана в операционной системе Windows";
+
+document.querySelector(".change__lang")
+.innerText="Для переключения языка комбинация: левыe shift + ctrl";
 }
 
 
